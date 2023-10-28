125512f9b54e4c0bb0cc9b08317115fe60442c93
CleanOak <53487230+CleanOak@users.noreply.github.com>
2023-10-28 11:09:56 +0100

    Add event listener to Dom and buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
-console.log("Helllo World");
+// Wait for DOM to finish running before game starts
+//get button elements
+
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function(){
+
+            let signType = this.getAttribute("sign-type");
+            alert(signType);
+            
+        })
+    }
+})
