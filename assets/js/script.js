d2633d8b946abc28342e86c5263f8d551247ee46
CleanOak <53487230+CleanOak@users.noreply.github.com>
2023-10-28 12:20:07 +0100

    Add computer choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,42 @@
 // Wait for DOM to finish running before game starts
 //get button elements
 
-document.addEventListener("DOMContentLoaded", function(){
-    let buttons = document.getElementsByTagName("button");
 
-    for (let button of buttons) {
+document.addEventListener("DOMContentLoaded", function(){
+        let buttons = document.getElementsByTagName("button");
+        for (let button of buttons) {
         button.addEventListener("click", function(){
 
             let signType = this.getAttribute("sign-type");
-            alert(signType);
+            yourSignChoice(signType);
+            computerChoice();
             
         })
     }
+    
 })
+
+function yourSignChoice(signType){
+    console.log("You have clicked on the " + signType);
+    
+}
+
+function computerChoice (){
+   let randomChoice = (Math.floor(Math.random() * 3));
+
+   if (randomChoice === 0){
+    console.log("rock");
+   }
+    else if (randomChoice === 1){
+        console.log("paper");
+    }
+       else if (randomChoice === 2){
+            console.log("scissors");
+        }   
+  
+}
+
+
+
+
+
