f0b06daf6cc1656415abc92ec256a3cf497618c6
CleanOak <53487230+CleanOak@users.noreply.github.com>
2023-10-30 17:41:28 +0000

    Add display computer and user choices with scores

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,63 +6,104 @@ let computerScore =0;
 let userScoreSpan = document.getElementById("user-score");
 let computerScoreSpan = document.getElementById("computer-score");
 let resultsComments = document.getElementById("display-comments");
+let userWeapon = document.getElementById("u-weapon");
+let computerWeapon = document.getElementById("c-weapon");
+
 
 document.addEventListener("DOMContentLoaded", function(){
         let buttons = document.getElementsByTagName("button");
         for (let button of buttons) {
 
-        button.addEventListener("click", function(){           
+        button.addEventListener("click", function(){     
+          if(this.getAttribute("sign-type") === "reset"){
+              //console.log("reset button");
+              reset();
+          } else {    
         let signType = this.getAttribute("sign-type");
         
        playGame(signType);
-    
+           }
         })    
     }  
 })
 
 function playGame(userChoice){
-  let compterChoice = getComputerChoice();
+  let computerChoice = getComputerChoice();
   
-  console.log('user choice =>', userChoice);
-  console.log('computer choice =>', compterChoice);
+ //display user weapon
+ if (userChoice === "rock"){
+  userWeapon.innerHTML = "rock";
+  userWeapon.style.color = "blue"
+ }
+ if (userChoice === "paper"){
+  userWeapon.innerHTML = "paper";
+  userWeapon.style.color = "blue"
+ }
+ if (userChoice === "scissors"){
+  userWeapon.innerHTML = "scissors";
+  userWeapon.style.color = "blue"
+ }
 
+ //display computer weapon
+ if (computerChoice === "rock"){
+  computerWeapon.innerHTML = "rock";
+  computerWeapon.style.color = "red"
+ }
+ if (computerChoice === "paper"){
+  computerWeapon.innerHTML = "paper";
+  computerWeapon.style.color = "red"
+ }
+ if (computerChoice === "sci"){
+  computerWeapon.innerHTML = "scissors";
+  computerWeapon.style.color = "red"
+ }
+  
   //draw conditions
-  if(userChoice === "rock" && compterChoice === "rock"){
+  if(userChoice === "rock" && computerChoice === "rock"){
     draw();
+   
   }
 
-  if(userChoice === "paper" && compterChoice === "paper"){
+  if(userChoice === "paper" && computerChoice === "paper"){
     draw();
+    
   }
 
-  if(userChoice === "scissors" && compterChoice === "sci"){
+  if(userChoice === "scissors" && computerChoice === "sci"){
     draw();
+    
   }
 
   //user win conditions
-  if(userChoice === "rock" && compterChoice === "sci"){
+  if(userChoice === "rock" && computerChoice === "sci"){
     youWin();
+   
   }
 
-  if(userChoice === "paper" && compterChoice === "rock"){
+  if(userChoice === "paper" && computerChoice === "rock"){
     youWin();
+    
   }
 
-  if(userChoice === "scissors" && compterChoice === "paper"){
+  if(userChoice === "scissors" && computerChoice === "paper"){
     youWin();
+   
   }
-  
+
   //computer win conditions
-  if(userChoice === "rock" && compterChoice === "paper"){
+  if(userChoice === "rock" && computerChoice === "paper"){
     compWin();
+   
   }
 
-  if(userChoice === "paper" && compterChoice === "sc"){
+  if(userChoice === "paper" && computerChoice === "sci"){
     compWin();
+    
   }
 
-  if(userChoice === "scissors" && compterChoice === "rock"){
+  if(userChoice === "scissors" && computerChoice === "rock"){
     compWin();
+    
   }
 
 }
@@ -90,6 +131,18 @@ resultsComments.textContent = "Computer Wins"
 function draw(){
 resultsComments.textContent = "It's A Draw!!!"
 }
+
+function reset(){
+   userScoreSpan.textContent = userScore;
+   computerScoreSpan.textContent = computerScore;
+
+}
+
+function weaponDisplay(){
+  let compterChoice = getComputerChoice;
+
+}
+
 
 
 
