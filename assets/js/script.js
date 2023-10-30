9bff3631ffacf01089fdb9ae21c4182ceb00d740
CleanOak <53487230+CleanOak@users.noreply.github.com>
2023-10-30 13:01:15 +0000

    Add results comments in main div

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,6 @@
 
 let userScore = 0;
 let computerScore =0;
-let comments = "";
 let userScoreSpan = document.getElementById("user-score");
 let computerScoreSpan = document.getElementById("computer-score");
 let resultsComments = document.getElementById("display-comments");
@@ -23,21 +22,73 @@ document.addEventListener("DOMContentLoaded", function(){
 
 function playGame(userChoice){
   let compterChoice = getComputerChoice();
-  console.log(userChoice);
+  
+  console.log('user choice =>', userChoice);
+  console.log('computer choice =>', compterChoice);
 
-  if(compterChoice === "rock"){
-    console.log("This is rock");
+  //draw conditions
+  if(userChoice === "rock" && compterChoice === "rock"){
+    draw();
+  }
+
+  if(userChoice === "paper" && compterChoice === "paper"){
+    draw();
+  }
+
+  if(userChoice === "scissors" && compterChoice === "sci"){
+    draw();
+  }
+
+  //user win conditions
+  if(userChoice === "rock" && compterChoice === "sci"){
+    youWin();
+  }
+
+  if(userChoice === "paper" && compterChoice === "rock"){
+    youWin();
+  }
+
+  if(userChoice === "scissors" && compterChoice === "paper"){
+    youWin();
+  }
+  
+  //computer win conditions
+  if(userChoice === "rock" && compterChoice === "paper"){
+    compWin();
+  }
+
+  if(userChoice === "paper" && compterChoice === "sc"){
+    compWin();
+  }
+
+  if(userChoice === "scissors" && compterChoice === "rock"){
+    compWin();
   }
 
 }
-
 
 function getComputerChoice (){
 
     let choices = ['rock', 'paper', 'sci']
     let randomChoice = (Math.floor(Math.random() * 3));
-    console.log(choices[randomChoice]);
+    return choices[randomChoice];
    
+}
+
+function youWin(){
+userScore++;
+userScoreSpan.textContent = userScore;
+resultsComments.textContent = "You Win";
+}
+
+function compWin(){
+computerScore++;
+computerScoreSpan.textContent = computerScore;
+resultsComments.textContent = "Computer Wins"
+}
+
+function draw(){
+resultsComments.textContent = "It's A Draw!!!"
 }
 
 
