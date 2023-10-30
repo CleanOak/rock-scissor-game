65f5224134b6df60b6ba5562a333115ab2273c66
CleanOak <53487230+CleanOak@users.noreply.github.com>
2023-10-30 12:27:32 +0000

    Add win, lose, draw functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,12 @@
 // Wait for DOM to finish running before game starts
 //get button elements
 
+let userScore = 0;
+let computerScore =0;
+let comments = "";
+let userScoreSpan = document.getElementById("user-score");
+let computerScoreSpan = document.getElementById("computer-score");
+let resultsComments = document.getElementById("display-comments");
 
 document.addEventListener("DOMContentLoaded", function(){
         let buttons = document.getElementsByTagName("button");
@@ -16,23 +22,75 @@ document.addEventListener("DOMContentLoaded", function(){
 })
 
 function playGame(userChoice){
-  let compterChoice = getComputerChoice();
-  console.log(userChoice);
+    let computerChoice = getComputerChoice();
+    
+    console.log('userChoice =>', userChoice)
+    console.log('computerChoice =>', computerChoice)
+  
+    //draw conditions
+    if(userChoice === "rock" && computerChoice === "rock"){
+      draw()
+    }
+    if(userChoice === "paper" && computerChoice === "paper"){
+      draw();
+    }
+  
+    if(userChoice === "scissors" && computerChoice === "sci"){
+      draw();
+    }
 
-  if(compterChoice === "rock"){
-    console.log("This is rock");
+    //user win conditions
+    if(userChoice === "rock" && computerChoice === "sci"){
+     youWin();
+    }
+    if(userChoice === "paper" && computerChoice === "rock"){
+      youWin();
+    }
+  
+    if(userChoice === "scissors" && computerChoice === "paper"){
+      youWin();
+    }
+  
+  //computer win conditions
+
+    if(userChoice === "rock" && computerChoice === "paper"){
+      compWin();
+    }
+    if(userChoice === "paper" && computerChoice === "sci"){
+      compWin();
+    }
+  
+    if(userChoice === "scissors" && computerChoice === "rock"){
+      compWin();
+    }
+  
+  }
+  
+  
+  function getComputerChoice (){
+  
+      let choices = ['rock', 'paper', 'sci']
+      let randomChoice = (Math.floor(Math.random() * 3));
+      return choices[randomChoice];
+     
   }
 
-}
+  function youWin(){
+    userScore++;
+    resultsComments.textContent = "You Win"
+    userScoreSpan.textContent = userScore;
 
+  }
 
-function getComputerChoice (){
+  function compWin(){
+    computerScore++;
+    resultsComments.textContent = "Computer Wins"
+    computerScoreSpan.textContent = computerScore;
+  }
 
-    let choices = ['rock', 'paper', 'sci']
-    let randomChoice = (Math.floor(Math.random() * 3));
-    console.log(choices[randomChoice]);
-   
-}
+  function draw(){
+    resultsComments.textContent = "It's a Draw!!!"
+  }
 
 
 
